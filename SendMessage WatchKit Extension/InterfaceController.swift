2007df8a431280291d623feee7992a9c5fc2ba0e
shoya <12c1055@gmail.com>
2015-05-13 18:42:52 +0900

    first commit

---
+++ SendMessage WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,55 @@
+//
+//  InterfaceController.swift
+//  SendMessage WatchKit Extension
+//
+//  Created by yuto on 2015/05/13.
+//  Copyright (c) 2015年 yuto. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var myLabel: WKInterfaceLabel!
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    var count = 0
+    @IBAction func tapBtn() {
+        count++
+        myLabel.setText("\(count)")
+        WKInterfaceController.openParentApplication(
+        ["fromWatchKit":"\(count)"],
+        reply:{replyInfo,error in
+            println(replyInfo["fromApp"]!)
+        })
+        
+        
+    }
+
+    @IBAction func tapBtn2() {
+        count--
+        myLabel.setText("\(count)")
+        WKInterfaceController.openParentApplication(
+            ["fromWatchKit":"\(count)"],
+            reply:{replyInfo,error in
+                println(replyInfo["fromApp"]!)
+        })
+    }
+
+}
---
+++ SendMessage/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SendMessage
+//
+//  Created by yuto on 2015/05/13.
+//  Copyright (c) 2015年 yuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+   
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
